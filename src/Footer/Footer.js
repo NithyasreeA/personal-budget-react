e11a3eccd10318cc16536d894b45cf2d5a163195
NithyAsree <natmakur@uncc.edu>
2024-09-29 19:33:00 -0400

    Brought Pbcode in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+
+
+    <div className="center" aria-label="COPYRIGHT INFORMATION">
+
+      All rights reserved &copy; Nithya Sree
+
+
+      <div className="h-card">
+
+        <p className="p-name">Nithya Sree</p>
+        &nbsp;
+        <p className="p-country-name">USA</p>
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
